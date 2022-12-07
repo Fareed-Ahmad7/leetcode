2ee6e10db790f31335eca4270b84f0039bc4958b
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-12-08 00:09:42 +0530

    second largest

---
+++ geekforgeeks/second largest/mySolution.py
@@ -0,0 +1,8 @@
+class Solution:
+	def print2largest(self,arr, n):
+        arr.sort()
+        second_larget_element = arr[-2]
+        return second_larget_element
+    
+
+link = "https://practice.geeksforgeeks.org/problems/second-largest3735/1"
